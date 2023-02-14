42f5dda791fcd7212cbcb823fd3933781a593d1a
Koldaf <koldaf20@gmail.com>
2023-02-14 18:22:25 +0100

    commited

--- js/app.js
+++ js/app.js
@@ -1,17 +1,58 @@
 
+function logIn(){
+  const username = $('#username').val();
+  if(username !== ''){
+    sessionStorage.setItem('username', username);
+    //check if there is a history on the user
+
+    
+      window.location.href = "#secondPage";
+    
+  }
+  return true;
+}
+
+function checkLogIn(){
+  const username = sessionStorage.getItem('username');
+  if(username === null){
+    window.location.href = "#firstPage";
+  }else{
+    //Display username
+    $('.userId').html(username);
+    //return to last save page
+    //get last known location
+    console.log(username);
+    //window.location.href = 
+  }
+}
+
+function curPage(){
+  const url = window.location.href;
+  
+  const parts = url.split('#');
+  if(parts[1] !== 'firstPage'){
+    localStorage.setItem('lastPage', parts[1]);
+  }
+  return parts[1];
+}
 
 $('#fullpage').fullpage({
     // options here
     licenseKey: 'gplv3-license',
     credits: { enabled: false, label: 'Developed by UbiLearn Consult', position: 'right'},
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
-    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+    sectionsColor: ['#f2f2f2', '#4BBFC3', '#4BBFC3', '#4BBFC3', '#4BBFC3', '#4BBFC3'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
     menu: '#myMenu',
   });
-  
+
 $('document').ready(function(){
     fullpage_api.setAllowScrolling(false, 'all');
     fullpage_api.setKeyboardScrolling(false, 'all');
     fullpage_api.setRecordHistory(false);
-    console.log('i am ready');
+    //check if user is on
+    checkLogIn();
+    
+    //save last page
+    curPage();
+  
 });
